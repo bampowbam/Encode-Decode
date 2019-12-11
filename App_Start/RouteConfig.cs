921c0766080d04ab1467bbfcf35352d2b490086f
Luke Balogun <lbalogun16@gmail.com>
2019-12-10 21:46:44 -0600

    Fixed up some ui validations

--- App_Start/RouteConfig.cs
+++ App_Start/RouteConfig.cs
@@ -16,9 +16,8 @@ namespace WebApplication1
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Default", action = "Encode", id = UrlParameter.Optional }
             );
         }
     }
 }
-
--- Controllers/DefaultController.cs
+++ Controllers/DefaultController.cs
@@ -2,69 +2,77 @@
 
 namespace WebApplication1.Controllers
 {
-  public class DefaultController : Controller
-  {
-
-    private int EncodeValue(int value)
+    public class DefaultController : Controller
     {
-      value += 8192;
-      return (value & 0x007F) + ((value & 0x3F80) << 1);
-    }
-
-    private int DecodeValue(byte high, byte low)
-    {
-      return (((high & 0x7F) << 7) + (low & 0x7F)) - 8192;
-    }
-
-    [HttpPost]
-    public ActionResult Encode(int value)
-    {
-      if (value >= -8192 && value < 8191)
-        return View((object)EncodeValue(value).ToString("X4").ToLower());
-      else
-        return View((object)null);
-    }
-
-    [HttpGet]
-    public ActionResult Encode()
-    {
-      return View();
-    }
-
-    private int? IntFromHex(string value)
-    {
-      string hex = "0123456789ABCDEF";
-      value = value.ToUpper();
-      int highIndex = hex.IndexOf(value[0]);
-      int lowIndex = hex.IndexOf(value[1]);
-      if (highIndex >= 0 && lowIndex >= 0)
-        return highIndex * 16 + lowIndex;
-      else
-        return null;
-    }
-
-    [HttpPost]
-    public ActionResult Decode(string highValue, string lowValue)
-    {
-      if (!string.IsNullOrEmpty(highValue) && !string.IsNullOrEmpty(lowValue) && highValue.Length == 2 && lowValue.Length == 2)
-      {
-        int? highValueAsInt = IntFromHex(highValue.ToUpper());
-        int? lowValueAsInt = IntFromHex(lowValue.ToUpper());
-        if (highValueAsInt.HasValue && lowValueAsInt.HasValue)
+        private ActionResult Index()
         {
-          return View((object)DecodeValue((byte)highValueAsInt.Value, (byte)lowValueAsInt.Value));
+            return View();
         }
-        else
-          return View((object)null);
-      }
-      else
-        return View((object)null);
-    }
+        private int EncodeValue(int value)
+        {
+            value += 8192;
+            return (value & 0x007F) + ((value & 0x3F80) << 1);
+        }
 
-    [HttpGet]
-    public ActionResult Decode()
-    {
-      return View();
+        private int DecodeValue(byte high, byte low)
+        {
+            return (((high & 0x7F) << 7) + (low & 0x7F)) - 8192;
+        }
+
+        [HttpPost]
+        public ActionResult Encode(string value)
+        {
+            if (int.TryParse(value, out int valueAsInt))
+            {
+                if (valueAsInt >= -8192 && valueAsInt < 8191)
+                    return View((object)EncodeValue(valueAsInt).ToString("X4").ToLower());
+                else
+                    return View((object)null);
+            }
+            else
+                return View((object)null);
+        }
+
+        [HttpGet]
+        public ActionResult Encode()
+        {
+            return View();
+        }
+
+        private int? IntFromHex(string value)
+        {
+            string hex = "0123456789ABCDEF";
+            value = value.ToUpper();
+            int highIndex = hex.IndexOf(value[0]);
+            int lowIndex = hex.IndexOf(value[1]);
+            if (highIndex >= 0 && lowIndex >= 0)
+                return highIndex * 16 + lowIndex;
+            else
+                return null;
+        }
+
+        [HttpPost]
+        public ActionResult Decode(string highValue, string lowValue)
+        {
+            if (!string.IsNullOrEmpty(highValue) && !string.IsNullOrEmpty(lowValue) && highValue.Length == 2 && lowValue.Length == 2)
+            {
+                int? highValueAsInt = IntFromHex(highValue.ToUpper());
+                int? lowValueAsInt = IntFromHex(lowValue.ToUpper());
+                if (highValueAsInt.HasValue && lowValueAsInt.HasValue)
+                {
+                    return View((object)DecodeValue((byte)highValueAsInt.Value, (byte)lowValueAsInt.Value));
+                }
+                else
+                    return View((object)null);
+            }
+            else
+                return View((object)null);
+        }
+
+        [HttpGet]
+        public ActionResult Decode()
+        {
+            return View();
+        }
     }
-  }
 }
